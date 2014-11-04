3bff0276ff7981384819b634e460d5a8c28d0f6f
Alvaro Graves <alvaro@graves.cl>
2014-11-03 16:37:27 -0800

    Generalization to "triple generators" as input:
    Each  widget will run a function that will create a series of triples. These triples will be added to the message object that will be sent to /create.
    
    Create will only ead the triples and add the to the insert query.

--- static/js/ontologyForm.js
+++ static/js/ontologyForm.js
@@ -5,76 +5,95 @@ ontologyFormApp.config(['$interpolateProvider', function($interpolateProvider) {
   $interpolateProvider.endSymbol(']]');
 }]);
 
-ontologyFormApp.controller('ontologyFormList', ['$scope', '$http', '$compile', function($scope, $http, $compile){	
+ontologyFormApp.controller('ontologyFormList', ['$scope', '$http', '$compile', function($scope, $http, $compile){
 	url = window.location;
 	$scope.instance = {};
-	var config = {headers:  
+	var config = {headers:
 		{
-        'Accept': 'application/json'
-    	}
-    };
+      'Accept': 'application/json'
+    }
+  };
 
-    $scope.urlify = function(u){
-    	return u.toLowerCase().replace(/[^a-zA-Z0-9\-]+/g, '_');
+  $scope.tripleGenerators = [];
+
+  $scope.urlify = function(u){
+   return u.toLowerCase().replace(/[^a-zA-Z0-9\-]+/g, '_');
+ }
+ $scope.uuid = function(){
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+});
+ }
+
+ $scope._createTextWidget = function(predicate, htmlElement){
+  var formElement = document.createElement("p");
+  var legend = document.createElement("label");
+  legend.innerHTML = predicate;
+  formElement.appendChild(legend);
+  var aux = document.createElement('input');
+  aux.type="text";
+  var id = $scope.uuid();
+  aux.setAttribute("id", id);
+  aux.setAttribute("class", "form-control");
+  aux.setAttribute("data-predicate", predicate);
+  aux.setAttribute("ng-model", "instance."+$scope.urlify(predicate)+".value");
+  $scope.instance[$scope.urlify(predicate)] = {"predicate": predicate}
+  formElement.appendChild(aux);
+  $compile(formElement)($scope);
+  var parent = document.getElementById(htmlElement).appendChild(formElement);
+
+  var _generator = {
+    predicate: predicate,
+    id:  id,
+    f: function(id, p){
+      return {s: $("#uri").val(), p: p, o: {value: $("#"+id).val(), type: "text"}};
     }
 
-    $scope.identifier = "";
-    $scope.baseNamespace = function(){return baseNamespace+$scope.urlify($scope.identifier)}
-    $scope.avisa = function(){
-    	msg = {};
-    	$.each($("input"), function(i, item){
-    		var p = $(item).attr("data-predicate");
-    		var v = $(item).val();
-    		if(msg[p] == undefined){
-    			msg[p] = [];
-    		}
-    		msg[p].push(v);
-    	} );
+  }
+  $scope.tripleGenerators.push(_generator);
+}
 
-    	console.log(msg);
-    	$http({url: '/create', 
-    		   data: msg,
-    		   method: "POST",
+$scope.identifier = "";
+$scope.baseNamespace = function(){return baseNamespace+$scope.urlify($scope.identifier)}
+$scope.letMeKnow = function(){
+ msg = {uri: $("#uri").val(), triples: []};
 
-    		}).
-  			success(function(data, status, headers, config) {
-  				alert("OK");
-  				console.log(data);
-  				console.log(status);
-  				console.log(headers);
-  				console.log(config);
-			}).
-  			error(function(data, status, headers, config) {
-  				alert("Error");
-  				console.log(status);
-			});
+ for(var i=0; i<$scope.tripleGenerators.length; i++){
+  var thisGenerator = $scope.tripleGenerators[i];
+  a = thisGenerator.f(thisGenerator.id, thisGenerator.predicate);
+  msg.triples = msg.triples.concat(a);
+ }
+ console.log(msg);
+ $http({url: '/create',
+   data: msg,
+   method: "POST",
+
+ }).
+ success(function(data, status, headers, config) {
+  alert("OK");
+}).
+ error(function(data, status, headers, config) {
+  alert("Error");
+});
+}
+$("#uriLabel").attr("data-predicate", labelPredicate);
+$http.get(url, config).success(function(data){
+  $scope.formData = data.main;
+  $scope.formData.forEach(function(datum){
+    if(datum.widget.value != "http://cognita.io/poderoEditor/layoutOntology/HTMLInputTextWidget"){
+      alert(datum.widget.value);
+    }else{
+      $scope._createTextWidget(datum.predicate.value, datum.htmlElement.value);
     }
-    $("#uriLabel").attr("data-predicate", labelPredicate);
-	$http.get(url, config).success(function(data){
-		$scope.formData = data.main;
-		$scope.formData.forEach(function(datum){
-			var formElement = document.createElement("p");
-			var legend = document.createElement("label");
-			legend.innerHTML = datum.predicate.curie;
-			formElement.appendChild(legend);
-			var aux = document.createElement('input');
-			aux.type="text";
-			aux.id=datum.predicate.value;
-			aux.setAttribute("class", "form-control");
-			aux.setAttribute("data-predicate", datum.predicate.value);
-			aux.setAttribute("ng-model", "instance."+$scope.urlify(datum.predicate.curie)+".value");
-			$scope.instance[$scope.urlify(datum.predicate.curie)] = {"predicate": datum.predicate.curie}
-			formElement.appendChild(aux);
-			$compile(formElement)($scope);
-  			var parent = document.getElementById(datum.htmlElement.value).appendChild(formElement);
-		});
-		var submit = document.createElement("submit");
-		submit.type="submit";
-		submit.setAttribute("class", "btn btn-primary");
-		submit.innerHTML = submitLabel;
-		submit.setAttribute("ng-click", "avisa()");
-		document.getElementById("myForm").appendChild(submit);
-		$compile(submit)($scope);
+  });
+  var submit = document.createElement("submit");
+  submit.type="submit";
+  submit.setAttribute("class", "btn btn-primary");
+  submit.innerHTML = submitLabel;
+  submit.setAttribute("ng-click", "letMeKnow()");
+  document.getElementById("myForm").appendChild(submit);
+  $compile(submit)($scope);
 
-	});
+});
 }])
