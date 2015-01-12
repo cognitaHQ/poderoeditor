16ed7b606cb699ae10006dad922a1764562b4b72
alangrafu <alvaro@graves.cl>
2015-01-12 01:00:06 -0800

    added -INT flag

--- installation.sh
+++ installation.sh
@@ -169,7 +169,7 @@ echo "cd $DB">> $END_SCRIPT
 echo "kill \`cat .pid\`">> $END_SCRIPT
 echo "#FLOD">> $END_SCRIPT
 echo "cd ../flod">> $END_SCRIPT
-echo "kill \`cat .pid\`">> $END_SCRIPT
+echo "kill -INT \`cat .pid\`">> $END_SCRIPT
 
 chmod +x $START_SCRIPT
 chmod +x $END_SCRIPT
